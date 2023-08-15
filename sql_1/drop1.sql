ee7b0257670df7582ff32a3e86c429fe3ae7c469
YANJUN_CHEN (Christopher) <50053539+1106601034@users.noreply.github.com>
2023-08-15 17:11:07 +1000

    a

--- sql_1/drop1.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE Loan_application;
-DROP TABLE Property;
-DROP TABLE bank_stuff;
-DROP TABLE Customer;
-DROP TABLE Saving_account;
-DROP TABLE Home_loan_account;
-SELECT table_name FROM user_tables;
--- sql_1/drop2.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE Transportation_;
-DROP TABLE Carrier_;
-DROP TABLE Stuff_;
-DROP TABLE Store_;
-DROP TABLE Carrier_owner_;
-DROP TABLE Tracking_statement_;
-DROP TABLE Order_;
-DROP TABLE Customer_;
-SELECT table_name FROM user_tables;
